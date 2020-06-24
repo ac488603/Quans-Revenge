d70c04966319d80568491f653fb4bb6f1c6ed6aa
Adam Castillo <ac488603@mail.fresnostate.edu>
2020-06-23 19:28:53 -0700

    fixed heroku port issue

--- index.js
+++ index.js
@@ -8,34 +8,33 @@ var room = [];
 var host = [];
 
 function generateRoomCode() {
-    var code = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var code = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    for (var i = 0; i < 5; i++)
+  for (var i = 0; i < 5; i++)
     code += possible.charAt(Math.floor(Math.random() * possible.length));
 
-    return code;
+  return code;
 }
 
 app.use(device.capture());
 
 //
 
-app.get('/', function(req, res){
-  if(req.device.type == "desktop") {
+app.get('/', function (req, res) {
+  if (req.device.type == "desktop") {
     app.use(express.static(__dirname + '/public'));
     res.sendFile(__dirname + '/public/index.html');
-  }
-  else if(req.device.type == "phone") {
+  } else if (req.device.type == "phone") {
     app.use(express.static(__dirname + '/public/phone'));
     res.sendFile(__dirname + '/public/phone/index.html');
   }
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   //console.log(socket.client.id + ' connected');
 
-  socket.on('coderequest', function() {
+  socket.on('coderequest', function () {
     var code = generateRoomCode();
     //console.log("new code: " + code);
     socket.join(code);
@@ -43,29 +42,28 @@ io.on('connection', function(socket){
     io.to(socket.id).emit('code', code);
   });
 
-  socket.on('input', function(inp) {
+  socket.on('input', function (inp) {
     io.to(room[socket.id]).emit('input', socket.id, inp);
   })
 
-  socket.on('join', function(code) {
-    if(host[code])  {
+  socket.on('join', function (code) {
+    if (host[code]) {
       //console.log("client joining " + code);
       io.to(socket.id).emit('accept');
       socket.join(code);
       room[socket.id] = code;
-    }
-    else {
+    } else {
       //console.log("room doesn't exist");
       io.to(socket.id).emit('reject');
     }
   });
 
-  socket.on('disconnect', function() {
-      console.log(socket.id + " disconnected");
+  socket.on('disconnect', function () {
+    console.log(socket.id + " disconnected");
   });
 });
 
 var port = 7777;
-http.listen(port, function(){
+http.listen(process.env.PORT || port, function () {
   console.log('listening on *:' + port);
 });
